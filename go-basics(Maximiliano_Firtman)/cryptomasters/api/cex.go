fe4cd96e45bcb56abab8a59471ed6b78d31637fc
LucaGiona <luca@senzproduction.com>
2025-04-03 17:02:31 +0200

    starting URL func

--- go-basics(Maximiliano_Firtman)/cryptomasters/api/cex.go
+++ go-basics(Maximiliano_Firtman)/cryptomasters/api/cex.go
@@ -1,7 +1,18 @@
 package api
 
-import "frontendmasters.com/go/crypto/datatypes"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"frontendmasters.com/go/crypto/datatypes"
+)
+
+const apiUrl = "https://cex.io/api/ticker/%s/EUR"
 
 func GetRate(currency string) (datatypes.Rate, error){
 	
+	upCurrency := strings.ToUpper(currency)
+	res, err := http.Get(fmt.Sprint(apiUrl, upCurrency))
+
 }
